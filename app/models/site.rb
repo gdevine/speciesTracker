d381b28094938621dfca18c01dcfc24ebec91b94
Gerard Devine <g.devine@westernsydney.edu.au>
2017-09-14 21:35:08 +1000

    Added seed data for real data

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,17 +1,17 @@
 class Site < ActiveRecord::Base
-  
+
   has_many :sightings, :class_name => 'Sighting', :foreign_key => 'site_id', :dependent => :restrict_with_exception
-  
+
   validates :name, :uniqueness => {:message => "This name already exists"}, :presence => { :message => "A name is required" }, length: { maximum: 80 }
   validates :street, length: { maximum: 80 }
   validates :suburb, :presence => { :message => "A suburb is required" }, length: { maximum: 80 }
   validates :centre_lat, :numericality => { :greater_than_or_equal_to => -90.0, :less_than_or_equal_to => 90 }
   validates :centre_lon, :numericality => { :greater_than_or_equal_to => 0.0, :less_than_or_equal_to => 360.0 }
-  validates :centre_alt, :numericality => { :greater_than_or_equal_to => 0.0, :less_than_or_equal_to => 10000.0 }
-  
+  validates :centre_alt, :numericality => { :greater_than_or_equal_to => 0.0, :less_than_or_equal_to => 10000.0 }, :allow_blank => true
+
   def listname
     # Returns the name and co-ords appended (for dropdown lists etc)
-    self.name.humanize << " (" << ApplicationController.helpers.to_twodp(self.centre_lat) << ", " << ApplicationController.helpers.to_twodp(self.centre_lon) << ")" 
+    self.name.humanize << " (" << ApplicationController.helpers.to_twodp(self.centre_lat) << ", " << ApplicationController.helpers.to_twodp(self.centre_lon) << ")"
   end
-  
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 require 'factory_girl'
+require 'csv'
 
 # Users
 
@@ -11,7 +12,26 @@ User.create(firstname:'Gerard-Super', surname:'Devine', email:'g.superuser@uws.e
 User.create(firstname:'Gerard-Admin', surname:'Devine', email:'g.admin@uws.edu.au', password:'qwertyui', password_confirmation:'qwertyui', approved:true, role:'admin')
 User.create(firstname:'Paul', surname:'Rymer', email:'p.rymer@uws.edu.au', password:'password', password_confirmation:'password', approved:true, role:'admin')
 User.create(firstname:'Desi', surname:'Quintans', email:'d.quintans@uws.edu.au', password:'password', password_confirmation:'password', approved:true, role:'admin')
-User.create(firstname:'Gerard-Unapproved', surname:'Devine', email:'g.unapproved@uws.edu.au', password:'qwertyui', password_confirmation:'qwertyui', approved:false, role:'user')
+User.create(firstname:'Demo', surname:'User_1', email:'demouser_1@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_2', email:'demouser_2@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_3', email:'demouser_3@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_4', email:'demouser_4@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_5', email:'demouser_5@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_6', email:'demouser_6@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_7', email:'demouser_7@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_8', email:'demouser_8@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_9', email:'demouser_9@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_10', email:'demouser_10@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_11', email:'demouser_11@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_12', email:'demouser_12@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_13', email:'demouser_13@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_14', email:'demouser_14@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_15', email:'demouser_15@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_16', email:'demouser_16@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_17', email:'demouser_17@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_18', email:'demouser_18@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_19', email:'demouser_19@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
+User.create(firstname:'Demo', surname:'User_20', email:'demouser_20@peastracker.com', password:'password', password_confirmation:'password', approved:true, role:'user')
 
 # Create some approved users
 5.times do
@@ -30,17 +50,74 @@ end
 FactoryGirl.create :admin
 
 
-# Create some species
-15.times do
-  FactoryGirl.create :species
+# Read actual Sites data from CSV file
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'peas_bats_sites.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Site.new
+  t.name = row['name']
+  t.street = row['street']
+  t.suburb = row['suburb']
+  t.comments = row['comments']
+  t.centre_lat = row['centre_lat']
+  t.centre_lon = row['centre_lon']
+  if !t.valid?
+    puts t.errors.full_messages
+    puts '******************'
+  else
+    t.save
+    puts "#{t.name}, #{t.street} saved"
+  end
 end
 
-# Create some sites
-15.times do
-  FactoryGirl.create :site
+puts "There are now #{Site.count} rows in the sites table"
+
+
+# Read actual species data from CSV file
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'peas_bats_species.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  s = Species.new
+  s.genus = row['genus']
+  s.species = row['species']
+  s.common_name = row['common_name']
+  s.description = row['description']
+  if !s.valid?
+    puts s.errors.full_messages
+    puts '******************'
+  else
+    s.save
+    puts "#{s.genus}, #{s.species} saved"
+  end
 end
 
-# Create sightings
-20.times do
-  FactoryGirl.create(:sighting, photo: File.new("#{Rails.root}/db/seed_photos/ST_Plant#{Random.rand(1..5).to_i.to_s}.jpg"))
+puts "There are now #{Species.count} rows in the species table"
+
+
+# Read actual sightings data from CSV file
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'peas_bats_sightings.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  p = Sighting.new
+  p.datetime_sighted = row['datetime_sighted'].to_datetime
+  p.species = Species.where(genus: row['genus'], species: row['species']).first
+  p.site = Site.where(street: row['street'], suburb: row['suburb']).first
+  p.spotter = User.where(firstname: 'Desi').first
+  p.creator = User.where(firstname: 'Desi').first
+  p.latitude = row['latitude']
+  p.longitude = row['longitude']
+  p.dom_reproductive_stage = row['dom_reproductive_stage']
+  p.healthy_flowers = row['healthy_flowers']
+  p.healthy_pods = row['healthy_pods']
+  p.adult_abundance = row['adult_abundance']
+  if !p.valid?
+    puts row['genus'], row['species']
+    puts p.errors.full_messages
+    puts '***************'
+  else
+    p.save
+    puts "#{p.species}, #{p.datetime_sighted} saved"
+  end
 end
+
+puts "There are now #{Sighting.count} rows in the sightings table"
